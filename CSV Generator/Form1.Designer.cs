c7d989b532adc72bc39aa165427c4dd1cec6cd01
Babak Dayyani <babak.dayyani@gmail.com>
2020-07-14 14:58:09 -0400

    Add project files.

---
+++ CSV Generator/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CSV_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button_Export = new MetroFramework.Controls.MetroButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button_browse = new MetroFramework.Controls.MetroButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_openfolder = new MetroFramework.Controls.MetroButton();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_Export
+            // 
+            this.Button_Export.Location = new System.Drawing.Point(49, 92);
+            this.Button_Export.Name = "Button_Export";
+            this.Button_Export.Size = new System.Drawing.Size(104, 23);
+            this.Button_Export.TabIndex = 0;
+            this.Button_Export.Text = "Export";
+            this.Button_Export.Click += new System.EventHandler(this.Button_Export_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 225);
+            this.listBox1.TabIndex = 22;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Load Images";
+            // 
+            // button_browse
+            // 
+            this.button_browse.Location = new System.Drawing.Point(101, 19);
+            this.button_browse.Name = "button_browse";
+            this.button_browse.Size = new System.Drawing.Size(75, 23);
+            this.button_browse.TabIndex = 21;
+            this.button_browse.Text = "Browse";
+            this.button_browse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_browse);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(23, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 288);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thermal Images";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_Name);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.button_openfolder);
+            this.groupBox2.Controls.Add(this.Button_Export);
+            this.groupBox2.Location = new System.Drawing.Point(214, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 168);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Export Options";
+            // 
+            // button_openfolder
+            // 
+            this.button_openfolder.Location = new System.Drawing.Point(49, 121);
+            this.button_openfolder.Name = "button_openfolder";
+            this.button_openfolder.Size = new System.Drawing.Size(104, 23);
+            this.button_openfolder.TabIndex = 6;
+            this.button_openfolder.Text = "Open Folder";
+            this.button_openfolder.Click += new System.EventHandler(this.button_openfolder_Click);
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(75, 24);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(119, 20);
+            this.textBox_Name.TabIndex = 30;
+            this.textBox_Name.Text = "ZRL_Skeleton";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(3, 28);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "File Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSV_Generator.Properties.Resources.QEA_Tech_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(320, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(441, 374);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "ZRL Skeleton Generator";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton Button_Export;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label9;
+        private MetroFramework.Controls.MetroButton button_browse;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Label label13;
+        private MetroFramework.Controls.MetroButton button_openfolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CSV Generator/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+using Flir.Atlas.Image;
+using System.Globalization;
+using MetroFramework;
+using CsvHelper;
+
+namespace CSV_Generator
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        public class ZRL_Info
+        {
+            public string Image { get; set; }
+            public int Zone { get; set; }
+            public int Riser { get; set; }
+            public int Level { get; set; }
+            public string Notes { get; set; }
+            public string Conclusion { get; set; }
+            public string Energy { get; set; }
+        }
+
+        public List<ZRL_Info> ZRL_List = new List<ZRL_Info>();
+
+        public string Source_Folder;
+        public string OutputFileName = "ZRL_Skeleton";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)
+        {
+
+            // Open all the thermal images
+            listBox1.Items.Clear();
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Open the folder containing Thermal images";
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Source_Folder = fbd.SelectedPath;
+                string[] files = Directory.GetFiles(Source_Folder);
+                foreach (string file in files)
+                {
+                    try
+                    {
+
+                        if (Flir.Atlas.Image.ThermalImageFile.IsThermalImage(file))
+                        {
+                            string name = file.Replace((Source_Folder + "\\"), "");
+                            name = name.Replace(".jpg", "");
+                            listBox1.Items.Add(name);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+            }
+            listBox1.SelectedIndex = 0;
+        }
+
+        public void UpdateZRL()
+        {
+
+            if (textBox_Name.Text != null && textBox_Name.Text != "")
+            {
+                OutputFileName = textBox_Name.Text;
+            }
+
+            string ExportFilePath = Source_Folder + "\\" + OutputFileName + ".csv";
+
+            try
+            {
+                if (File.Exists(ExportFilePath))
+                    File.Delete(ExportFilePath);
+
+                int N = listBox1.Items.Count;
+                //string Final_Report = "";
+                for (int index = 0; index < N; index++)
+                {
+                    string Name = listBox1.Items[index].ToString();
+                    ZRL_Info temp = new ZRL_Info();
+                    temp.Image = Name;
+                    ZRL_List.Add(temp);
+                    Console.WriteLine(index);
+                }
+
+                using (var writer = new StreamWriter(ExportFilePath))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(ZRL_List);
+                }
+
+
+                MessageBox.Show("Export is Done\r\nOpen Folder to find the report", "Export Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception EX)
+            {
+                string Error = EX.ToString();
+                if (Error.Contains("is being used by another process"))
+                {
+                    MessageBox.Show("Error: \rTargt file is open\rClose the target file", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Error:\r\b" + EX, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+
+        }
+
+        private void Button_Export_Click(object sender, EventArgs e)
+        {
+            UpdateZRL();
+        }
+
+        private void button_openfolder_Click(object sender, EventArgs e)
+        {
+            Process.Start(Source_Folder);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            GC.Collect();
+            Application.Exit();
+        }
+    }
+}
